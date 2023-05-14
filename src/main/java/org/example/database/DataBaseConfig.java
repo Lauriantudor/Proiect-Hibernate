8bf8bd56111c1d5f758e35619dd4dd4e48873832
Lauriantudor <52708705+Lauriantudor@users.noreply.github.com>
2023-05-14 11:57:54 +0300

    Data base Config

---
+++ src/main/java/org/example/database/DataBaseConfig.java
@@ -0,0 +1,21 @@
+package org.example.database;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DataBaseConfig {
+    private static SessionFactory sessionFactory=null;
+
+    private DataBaseConfig() {
+    }
+
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory == null){
+            sessionFactory= new Configuration()
+                    .configure("hibernate.config.xml")
+
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+}
